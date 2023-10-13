04aa7e521f3d48a5d00e2841d5967648505387bb
mengwei <mengwei@example.com>
2023-10-12 20:08:19 -0700

    added data to template from model

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 #post_list方法接受 request 参数作为输入， 并 return （返回）用 render 方法渲染模板 blog/post_list.html
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #返回request, template地址，{}中则是template需要的东西
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
